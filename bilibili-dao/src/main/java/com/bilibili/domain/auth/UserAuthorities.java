281f525522865364f3a4f077feabc70b65defb6a
charles2secret <charles2secret@gmail.com>
2022-04-01 17:42:22 -0700

    Finish all databases and coresponding Java classes for RBAC (role-based-action-control) model

---
+++ bilibili-dao/src/main/java/com/bilibili/domain/auth/UserAuthorities.java
@@ -0,0 +1,4 @@
+package com.bilibili.domain.auth;
+
+public class UserAuthorities {
+}
---
+++ bilibili-dao/src/main/java/com/bilibili/domain/auth/UserRole.java
@@ -0,0 +1,47 @@
+package com.bilibili.domain.auth;
+
+import java.util.Date;
+import java.util.zip.DataFormatException;
+
+public class UserRole {
+
+    private Long id;
+
+    private Long userId;
+
+    private Long roleId;
+
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
